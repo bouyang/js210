1f24ecdbbf0a8137f7cf3e23ca94a26c04019362
Brian Ouyang <ouyango@gmail.com>
2024-02-13 20:40:24 -0800

    Changed folder name

--- practice_problems/19_rot13_cipher.js
+++ practice_problems/19_rot13_cipher.js
@@ -36,6 +36,37 @@ console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.
 // logs:
 //Teachers open the door, but you must enter by yourself.
 
+
+function rot13_v2(input, offset) {
+  const LETTERS_IN_ALPHABET = 26;
+  const UPPERCASE_ASCII_MAX = 90;
+  const LOWERCASE_ASCII_MAX = 122;
+
+  if (offset === undefined) {
+    offset = 0;
+  }
+
+  let result = '';
+
+  let new_offset = offset % LETTERS_IN_ALPHABET;
+
+  for (let index = 0; index < input.length; index += 1) {
+    let charCode = input.charCodeAt(index);
+
+    if (isAlpha(input[index])) {
+      charCode += new_offset;
+
+      if ((isUppercase(input[index]) && charCode > UPPERCASE_ASCII_MAX) || (isLowercase(input[index]) && charCode > LOWERCASE_ASCII_MAX)) {
+        charCode -= LETTERS_IN_ALPHABET;
+      }
+    }
+    
+    result += String.fromCharCode(charCode);
+  }
+
+  return result;
+}
+
 function isUppercase(letter) {
   return (letter >= 'A' && letter <= 'Z');
 }
@@ -48,33 +79,6 @@ function isAlpha(letter) {
   return (isUppercase(letter) || isLowercase(letter));
 }
 
-function rot13_2(input, offset) {
-  const LETTERS_IN_ALPHABET = 26;
-  const UPPERCASE_ASCII = 90;
-  const LOWERCASE_ASCII = 122;
+console.log(rot13_v2('Teachers open the door, but you must enter by yourself.', 13));
 
-  if (offset === undefined) {
-    offset = 0;
-  }
-
-  let result = '';
-  new_offset = offset % LETTERS_IN_ALPHABET;
-
-  for (let index = 0; index < input.length; index += 1) {
-    let charCode = input.charCodeAt(index);
-
-    if (isAlpha(input[index])) {
-      charCode += new_offset;
-
-      if ((isUppercase(input[index]) && charCode > UPPERCASE_ASCII) || (isLowercase(input[index]) && charCode > LOWERCASE_ASCII)) {
-        charCode -= LETTERS_IN_ALPHABET;
-      }
-    }
-    
-    result += String.fromCharCode(charCode);
-  }
-
-  return result;
-}
-
-console.log(rot13_2('Teachers open the door, but you must enter by yourself.', 39));
+console.log(rot13_v2(rot13_v2('Teachers open the door, but you must enter by yourself.', 13), 39));
