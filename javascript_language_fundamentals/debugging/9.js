121780a8d70d7120c6e5f0bbdaa169e4b3c0c01d
Brian Ouyang <ouyango@gmail.com>
2024-02-16 22:40:33 -0800

    Add js fundamentals debugging exercises

---
+++ javascript_language_fundamentals/debugging/9.js
@@ -0,0 +1,28 @@
+const totalPages = 364;
+let energy = 100;
+let currentPage = 1;
+
+function read() {
+
+
+  while (energy > 0 && currentPage < totalPages) {
+    currentPage += 1;
+    energy -= (5 + currentPage * 0.1);
+  }
+
+  console.log(`Made it to page ${String(currentPage)}.`);
+
+  // Drink a cup of coffee.
+  energy = 100;
+
+  // Continue reading.
+  if (currentPage < totalPages) {
+    read();
+  } else {
+    console.log('Done!');
+  }
+}
+
+read();
+
+// currentPage keeps reseting to 1 whenever the read function is called on line 19
